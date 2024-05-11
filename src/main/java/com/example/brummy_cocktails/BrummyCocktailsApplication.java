f3b25b3ff6253cf5ee7458603f2e304869bc1f37
J Benoist <jb931@student.le.ac.uk>
2024-05-11 03:27:15 +0100

    Initial commit

---
+++ src/main/java/com/example/brummy_cocktails/BrummyCocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.brummy_cocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrummyCocktailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrummyCocktailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/brummy_cocktails/BrummyCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brummy_cocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrummyCocktailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
